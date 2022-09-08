99629977772bf9d95a58d26c009ace681e2322fb
susannawerth <swerth@vt.edu>
2022-09-07 22:47:39 -0400

    L06

--- L06/SetSnippet.py
+++ L06/SetSnippet.py
@@ -11,30 +11,32 @@ restaurantsInOurTown = [
     'Our Daily Bread',
     'Olive Garden',
     'Pommes de Terre',
-    'Jean"s''LaBoca', 
+    'Jean"s',
+    'LaBoca', 
     'Pont Blanc', 
-    'La Madeleine', 
     'Berliner Kueche', 
-    'La Madeleine',
     'Olive Garden']
 
-setFrench = set(['La Madeleine','Our Daily Bread','Jean"s'])
+setFrench = set(['La Madeleine','Our Daily Bread','Pommes de Terre','Pont Blanc'])
 setPark = set(['LaBoca', 'La Madeleine', 'Berliner Kueche', 'Olive Garden'])
 
 
 # Is the restaurant 'Madeleine' located near a park?
 
 
-# Are french restaurants a category of park restaurants?
+# Are all french restaurants also park restaurants?
 
 
-# Are park restaurants a category of french restaurants?
+# Are all park restaurants also french restaurants?
 
 
-# We are just hungry, any restaurant is fine!
+# We are just hungry, any restaurant that is either french or at a park is fine!
 
 
 # Find a french restaurant near a park!
 
 
 # Find a restaurant that is near a park but not french style!
+
+
+# My friend does not like french and near a park is not fancy enough. Which place can we go?
