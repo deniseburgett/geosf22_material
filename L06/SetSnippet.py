fe797068fe9260ef53fd1396836a6b7c139f5bdb
susannawerth <swerth@vt.edu>
2022-09-07 22:32:18 -0400

    L06

---
+++ L06/SetSnippet.py
@@ -0,0 +1,40 @@
+##############################
+# load code into a jupyter notebook with the magic command %load:
+# %load ../geosf22_material/L06/SetSnippet.py 
+
+# or without file path:
+# %load SetSnippet.py 
+##############################
+
+restaurantsInOurTown = [
+    'La Madeleine',
+    'Our Daily Bread',
+    'Olive Garden',
+    'Pommes de Terre',
+    'Jean"s''LaBoca', 
+    'Pont Blanc', 
+    'La Madeleine', 
+    'Berliner Kueche', 
+    'La Madeleine',
+    'Olive Garden']
+
+setFrench = set(['La Madeleine','Our Daily Bread','Jean"s'])
+setPark = set(['LaBoca', 'La Madeleine', 'Berliner Kueche', 'Olive Garden'])
+
+
+# Is the restaurant 'Madeleine' located near a park?
+
+
+# Are french restaurants a category of park restaurants?
+
+
+# Are park restaurants a category of french restaurants?
+
+
+# We are just hungry, any restaurant is fine!
+
+
+# Find a french restaurant near a park!
+
+
+# Find a restaurant that is near a park but not french style!
